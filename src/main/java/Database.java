6452027254f67149345ae36b00247b5fd6f6da38
TOMMASO26175 <tommasoarioli2@hotmail.com>
2022-03-03 18:50:58 +0100

    get req and fixes

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -2,30 +2,19 @@ import java.sql.*;
 
 
 public class Database {
-    public static void main(String[] args) {
+    public static String[] getData(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch ( ClassNotFoundException e ) {
             e.printStackTrace();
         }
-        String isbn = "FJALSS";
         final String DB_URL = "jdbc:mysql://rds-mysql-main.ck4bf8ke7zt9.eu-central-1.rds.amazonaws.com/School";
         final String USER = "admin";
         final String PASS = "Ym4&lrSyAGv";
-        final String QUERY = "SELECT Autore,Titolo FROM Libri WHERE ISBN = ?";
-        String username = null;
-
-        try (
-                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                PreparedStatement pstmt = conn.prepareStatement( QUERY )
-        )
-        {
-            Library library = new Library();
-            System.out.println(library.read(conn));
-        } catch ( SQLException e ) {
-            e.printStackTrace();
-        }
-
-
+        String[] data = new String[3];
+        data[0] = DB_URL;
+        data[1] = USER;
+        data[2] = PASS;
+        return data;
     }
 }
--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,16 +1,27 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import com.google.gson.Gson;
+import com.sun.tools.javac.comp.Todo;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+@Path("/api")
 public class Library {
-
-    public List<Book> read(Connection conn){
+    @GET
+    @Path("/books")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response read(){
         final String QUERY = "SELECT Autore,Titolo,ISBN FROM Libri";
         final List<Book> books = new ArrayList<Book>();
+        final String[] data = Database.getData();
         try(
+
+                Connection conn = DriverManager.getConnection(data[0], data[1], data[2]);
                 PreparedStatement pstmt = conn.prepareStatement( QUERY )
         ) {
             ResultSet results =  pstmt.executeQuery();
@@ -24,6 +35,7 @@ public class Library {
         }catch (SQLException e){
             e.printStackTrace();
         }
-        return books;
+        String obj = new Gson().toJson(books);
+        return Response.status(200).entity(obj).build();
     }
 }
