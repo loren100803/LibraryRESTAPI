de9c9909c3c29f18b9e40e1ede15bfa8d0d7efd2
TOMMASO26175 <51451104+TOMMASO26175@users.noreply.github.com>
2022-03-04 12:46:36 +0100

    final rest library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,23 +1,25 @@
 import com.google.gson.Gson;
-import com.sun.tools.javac.comp.Todo;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-@Path("/api")
+@Path("/api/book")
 public class Library {
+    private final String error = "Server error, contact administrators";
+    private boolean checkParams(String isbn,String autore, String titolo){
+        return (isbn == null || isbn.trim().length() == 0) || (titolo == null || titolo.trim().length() == 0) || (autore == null || autore.trim().length() == 0);
+    }
+
     @GET
-    @Path("/books")
+    @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
     public Response read(){
-        final String QUERY = "SELECT Autore,Titolo,ISBN FROM Libri";
-        final List<Book> books = new ArrayList<Book>();
+        final String QUERY = "SELECT * FROM Libri";
+        final List<Book> books = new ArrayList<>();
         final String[] data = Database.getData();
         try(
 
@@ -31,11 +33,103 @@ public class Library {
                 book.setAutore(results.getString("Autore"));
                 book.setISBN(results.getString("ISBN"));
                 books.add(book);
+
             }
         }catch (SQLException e){
             e.printStackTrace();
+            String obj = new Gson().toJson(error);
+            return Response.serverError().entity(obj).build();
         }
         String obj = new Gson().toJson(books);
         return Response.status(200).entity(obj).build();
+    }
+
+    @PUT
+    @Path("/update")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response update(@FormParam("ISBN") String isbn,
+                           @FormParam("Titolo")String titolo,
+                           @FormParam("Autore") String autore){
+        if(checkParams(isbn, titolo, autore)) {
+            String obj = new Gson().toJson("Parameters must be valid");
+            return Response.serverError().entity(obj).build();
+        }
+        final String QUERY = "UPDATE Libri SET Titolo = ?, Autore = ? WHERE ISBN = ?";
+        final String[] data = Database.getData();
+        try(
+
+                Connection conn = DriverManager.getConnection(data[0], data[1], data[2]);
+                PreparedStatement pstmt = conn.prepareStatement( QUERY )
+        ) {
+            pstmt.setString(1,titolo);
+            pstmt.setString(2,autore);
+            pstmt.setString(3,isbn);
+            pstmt.execute();
+        }catch (SQLException e){
+            e.printStackTrace();
+            String obj = new Gson().toJson(error);
+            return Response.serverError().entity(obj).build();
+        }
+        String obj = new Gson().toJson("Libro con ISBN:" + isbn + " modificato con successo");
+        return Response.ok(obj,MediaType.APPLICATION_JSON).build();
+    }
+
+    @POST
+    @Path("/add")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response create(@FormParam("ISBN") String isbn,
+                           @FormParam("Titolo")String titolo,
+                           @FormParam("Autore") String autore){
+        if(checkParams(isbn, titolo, autore)) {
+            String obj = new Gson().toJson("Parameters must be valid");
+            return Response.serverError().entity(obj).build();
+        }
+        final String QUERY = "INSERT INTO Libri(ISBN,Titolo,Autore) VALUES(?,?,?)";
+        final String[] data = Database.getData();
+        try(
+
+                Connection conn = DriverManager.getConnection(data[0], data[1], data[2]);
+                PreparedStatement pstmt = conn.prepareStatement( QUERY )
+        ) {
+            pstmt.setString(1,isbn);
+            pstmt.setString(2,autore);
+            pstmt.setString(3,titolo);
+            pstmt.execute();
+        }catch (SQLException e){
+            e.printStackTrace();
+            String obj = new Gson().toJson(error);
+            return Response.serverError().entity(obj).build();
+        }
+        String obj = new Gson().toJson("Libro con ISBN:" + isbn + " aggiunto con successo");
+        return Response.ok(obj,MediaType.APPLICATION_JSON).build();
+    }
+
+    @DELETE
+    @Path("/delete")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response update(@FormParam("ISBN") String isbn){
+        if(isbn == null || isbn.trim().length() == 0){
+            String obj = new Gson().toJson("ISBN must be valid");
+            return Response.serverError().entity(obj).build();
+        }
+        final String QUERY = "DELETE FROM Libri WHERE ISBN = ?";
+        final String[] data = Database.getData();
+        try(
+
+                Connection conn = DriverManager.getConnection(data[0], data[1], data[2]);
+                PreparedStatement pstmt = conn.prepareStatement( QUERY )
+        ) {
+            pstmt.setString(1,isbn);
+            pstmt.execute();
+        }catch (SQLException e){
+            e.printStackTrace();
+            String obj = new Gson().toJson(error);
+            return Response.serverError().entity(obj).build();
+        }
+        String obj = new Gson().toJson("Libro con ISBN:" + isbn + " eliminato con successo");
+        return Response.ok(obj,MediaType.APPLICATION_JSON).build();
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
     <body>
-        <h2>Hello World!</h2>
+        <h2>Basic Rest Library</h2>
     </body>
 </html>
